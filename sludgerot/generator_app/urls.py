1f85bbac52885e0fd38a8baf6fad87adfa9d52a5
dustysi <dustinsilvaer@gmail.com>
2023-10-21 13:41:32 -0700

    pushed new changes to old code

--- sludgerot/generator_app/urls.py
+++ sludgerot/generator_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('generate_video/', views.generate_video, name='generate_video'),
+    path('home/', views.home, name='home'),
+    # path('generate_video/', views.generate_video, name='generate_video'),
 ]
--- sludgerot/sludgerot/settings.py
+++ sludgerot/sludgerot/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,6 +136,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Specify the base directory for media file uploads
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Specify the URL to access media files in the browser
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
